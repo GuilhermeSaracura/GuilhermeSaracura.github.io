1d9d91e1a82540b984a8044498d6587cfecaa4ff
Guilherme Saracura <guilhermesaracura@gmail.com>
2022-04-25 23:54:38 -0300

    Refactoring the code for using modules (import and export)

---
+++ js/languages/english.js
@@ -0,0 +1,136 @@
+export const englishLanguage = {
+    //Navbar texts
+    NAVBAR_HOME: { id: "#navbar-home", value: "Home" },
+    NAVBAR_ABOUT: { id: "#navbar-about", value: "About" },
+    NAVBAR_RESUME: { id: "#navbar-resume", value: "Resume" },
+    NAVBAR_SKILLS: { id: "#navbar-skills", value: "Skills" },
+    NAVBAR_PORTFOLIO: { id: "#navbar-portfolio", value: "Portfolio" },
+    NAVBAR_CONTACTS: { id: "#navbar-contacts", value: "Contacts" },
+    
+    //Home Script
+    NAVBAR_TYPED_SCRIPT: { id: "#typed-script", value: "Computer Engineering student, looking to become a Software and Web Developer." },
+
+    //About Section
+    ABOUT_TITLE: { id: "#about-title", value: "About" },
+    ABOUT_OBJECTIVE: { id: "#about-objective", value: "Professional Objective: Work as a Software and Web Developer." },
+    ABOUT_DESCRIPTION: { id: "#about-description", value: "Currently pursuing a degree in Computer Engineering at FACENS. Working as an intern in Software Development at FIT-Instituto de Tecnologia, where I participate in software development using C#.NET and WPF. One year and six months of experience in the IT area, performing in the management of activities, maintenance and assembly of equipment, Windows operating system users support and network infrastructure maintenance and support." },
+    ABOUT_BIRTH_DATE: { id: "#about-birth-date", value: "Birth Date:" },
+    ABOUT_DATE: { id: "#about-date", value: "May 27, 1996" },
+    ABOUT_CELLPHONE: { id: "#about-cellphone", value: "Cell phone:" },
+    ABOUT_CITY: { id: "#about-city", value: "City:" },
+    ABOUT_CITY_INFO: { id: "#about-city-info", value: "Sorocaba-SP Brazil" },
+    ABOUT_DEGREE: { id: "#about-degree", value: "Academic background:" },
+    ABOUT_DEGREE_INFO: { id: "#about-degree-info", value: "Computer Engineering" },
+
+    //Resume section
+    RESUME_TITLE: { id: "#resume-title", value: "Resume" },
+    RESUME_PROFESSIONAL_EXPERIENCE: { id: "#resume-professional-experience", value: "Professional Experience" },
+    RESUME_INTERN_DEV: { id: "#resume-intern-dev", value: "Software Development Intern" },
+    RESUME_INTERN_DEV_DATE: { id: "#resume-intern-dev-date", value: "May/2021 - Current Job" },
+    RESUME_INTERN_DEV_DESCRIPTION: { id: "#resume-intern-dev-description", value: "Performing tasks related to software development in C# and WPF languages, fixing bugs and performing tests, supporting the software development team, following the routine with the Scrum methodology in performing tasks and participating in alignment and planning meetings." },
+    RESUME_IT_ANALYST: { id: "#resume-it-analyst", value: "IT Analyst" },
+    RESUME_IT_ANALYST_DATE: { id: "#resume-it-analyst-date", value: "January/2021 - April/2021" },
+    RESUME_IT_ANALYST_DESCRIPTION: { id: "#resume-it-analyst-description", value: "Windows operating system users support, server maintenance and monitoring, network infrastructure maintenance and support, budgeting for equipment purchases, equipment maintenance and assembly." },
+    RESUME_IT_ASSISTANT: { id: "#resume-it-assistant", value: "IT Assistant" },
+    RESUME_IT_ASSISTANT_DATE: { id: "#resume-it-assistant-date", value: "February/2020 - December/2020" },
+    RESUME_IT_ASSISTANT_DESCRIPTION: { id: "#resume-it-assistant-description", value: "Maintenance and assembly of equipment, Windows operating system users support, and network infrastructure maintenance and support." },
+    RESUME_IT_INTERN: { id: "#resume-it-intern", value: "IT Intern" },
+    RESUME_IT_INTERN_DATE: { id: "#resume-it-intern-date", value: "November/2019 - January/2020" },
+    RESUME_IT_INTERN_DESCRIPTION: { id: "#resume-it-intern-description", value: "Technical assistance in systems, users support, and support for the IT team." },
+    RESUME_ACADEMIC_BACKGROUND: { id: "#resume-academic-background", value: "Academic Background" },
+    RESUME_COMPUTER_ENGINEERING: { id: "#resume-degree-ce", value: "Bachelor in Computer Engineering - FACENS" },
+    RESUME_COMPUTER_ENGINEERING_DATE: { id: "#resume-degree-ce-date", value: "June/2019 - Forecast of completion: December/2023" },
+    RESUME_COMPUTER_ENGINEERING_lOCALIZATION: { id: "#resume-degree-ce-localization", value: "Sorocaba-SP Brazil" },
+    RESUME_LEAN_SIX_SIGMA: { id: "#resume-degree-lss", value: "Postgraduate Specialization in Lean Six Sigma with Black Belt certification - FACENS" },
+    RESUME_LEAN_SIX_SIGMA_DATE: { id: "#resume-degree-lss-date", value: "March/2019 - June/2021" },
+    RESUME_LEAN_SIX_SIGMA_lOCALIZATION: { id: "#resume-degree-lss-localization", value: "Sorocaba-SP Brazil" },
+    RESUME_CHEMICAL_ENGINEERING: { id: "#resume-degree-qe", value: "Bachelor in Chemical Engineering - FACENS" },
+    RESUME_CHEMICAL_ENGINEERING_DATE: { id: "#resume-degree-qe-date", value: "January/2014 - December/2018" },
+    RESUME_CHEMICAL_ENGINEERING_lOCALIZATION: { id: "#resume-degree-qe-localization", value: "Sorocaba-SP Brazil" },
+    RESUME_COURSES: { id: "#resume-courses", value: "Courses" },
+    RESUME_COURSES_CSHARP_HOURS: { id: "#resume-courses-csharp_hours", value: "(37 hours)" },
+    RESUME_COURSES_CSHARP_CERTIFICATE: { id: "#resume-courses-csharp-certificate", value: " Certificate" },
+    RESUME_COURSES_LINUX_HOURS: { id: "#resume-courses-linux_hours", value: "(8,5 hours)" },
+    RESUME_COURSES_LINUX_CERTIFICATE: { id: "#resume-courses-linux-certificate", value: " Certificate" },
+
+    //Skills section
+    SKILLS_TITLE: { id: "#skills-title", value: "Skills" },
+    SKILLS_FRAMEWORKS_PROGRAMING: { id: "#skills-frameworks-programing", value: "Knowledge of Programming Languages and Frameworks." },
+    SKILLS_HTML: { id: "#skills-html", value: " - Intermediary" },
+    SKILLS_CSS: { id: "#skills-css", value: " - Intermediary" },
+    SKILLS_CSHARP: { id: "#skills-csharp", value: " - Intermediary" },
+    SKILLS_C:{ id: "#skills-c", value: " - Basic" },
+    SKILLS_JS:{ id: "#skills-js", value: " - Basic" },
+    SKILLS_JAVA: { id: "#skills-java", value: " - Intermediary" },
+    SKILLS_ANGULAR:{ id: "#skills-angular", value: " - Basic" },
+    SKILLS_VUE:{ id: "#skills-vue", value: " - Basic" },
+    SKILLS_WPF:{ id: "#skills-wpf", value: " - Basic" },
+    SKILLS_TOOL_KNOWLEDGE: { id: "#skills-tool-knowledge", value: "Knowledge in Development Tools." },
+    SKILLS_GIT: { id: "#skills-git", value: " - Intermediary" },
+    SKILLS_VSCODE: { id: "#skills-vscode", value: " - Intermediary" },
+    SKILLS_SCRUM:{ id: "#skills-scrum", value: " - Basic" },
+    SKILLS_VS:{ id: "#skills-vs", value: " - Intermediary" },
+    SKILLS_LANGUAGES: { id: "#skills-languages", value: "Languages." },
+    SKILLS_PT:{ id: "#skills-pt", value: " - Native" },
+    SKILLS_EN: { id: "#skills-en", value: " - Intermediary" },
+
+    //Portfolio section
+    PORTFOLIO_TITLE: { id: "#portfolio-title", value: "Portfolio" },
+    PORTFOLIO_SUBTITLE: { id: "#portfolio-subtitle", value: "Projects and works made from courses, personal projects and Computer Engineering course subjects." },
+    PORTFOLIO_SOFTWARES: { id: "#portfolio-softwares", value: "-Software Projects" },
+    PORTFOLIO_WEB: { id: "#portfolio-web", value:"-Web Application Projects" },
+    
+    //Contacts section
+    CONTACTS_TITLE: { id: "#contacts-title", value: "Contacts" },
+    CONTACTS_SUBTITLE: { id: "#contacts-subtitle", value: "Here are my personal contacts:" },
+    CONTACTS_LOCALIZATION: { id: "#contacts-localization", value: "Localization:" },
+    CONTACTS_LOCALIZATION_INFO: { id: "#contacts-localization-info", value: "Sorocaba-SP Brazil" },
+    CONTACTS_CELLPHONE: { id: "#contacts-cellphone", value: "Cell phone:" },
+
+    //Software Portfolio
+    P_SOFTWARE_TITLE: { id: "#software-portfolio-title", value: "Software Projects" },
+    P_SOFTWARE_CHESS: { id: "#software-portfolio-chess", value: "Chess Project" },
+    P_SOFTWARE_CHESS_DESCRIPTION: { id: "#software-portfolio-chess-description", value: "Project developed during the C# COMPLETO Programação Orientada a Objetos + Projetos course, simulating the classic chess game. Applying the knowledge of encapsulation, inheritance, polymorphism, enumeration and exception handling." },
+    P_SOFTWARE_CHESS_DIRECTORY: { id: "#software-portfolio-directory", value: "Directory" },    
+
+}
+
+export const englishLanguagesoft = {
+
+    //Navbar texts
+    NAVBAR_HOME: { id: "#navbar-home", value: "Home" },
+    NAVBAR_ABOUT: { id: "#navbar-about", value: "About" },
+    NAVBAR_RESUME: { id: "#navbar-resume", value: "Resume" },
+    NAVBAR_SKILLS: { id: "#navbar-skills", value: "Skills" },
+    NAVBAR_PORTFOLIO: { id: "#navbar-portfolio", value: "Portfolio" },
+    NAVBAR_CONTACTS: { id: "#navbar-contacts", value: "Contacts" },
+
+    //Software Portfolio
+    P_SOFTWARE_TITLE: { id: "#software-portfolio-title", value: "Software Projects" },
+    P_SOFTWARE_CHESS: { id: "#software-portfolio-chess", value: "Chess Project" },
+    P_SOFTWARE_CHESS_DESCRIPTION: { id: "#software-portfolio-chess-description", value: "Project developed during the C# COMPLETO Programação Orientada a Objetos + Projetos course, simulating the classic chess game. Applying the knowledge of encapsulation, inheritance, polymorphism, enumeration and exception handling." },
+    P_SOFTWARE_CHESS_DIRECTORY: { id: "#software-portfolio-directory", value: "Directory" },   
+}
+
+export const englishLanguageweb = {
+
+    //Navbar texts
+    NAVBAR_HOME: { id: "#navbar-home", value: "Home" },
+    NAVBAR_ABOUT: { id: "#navbar-about", value: "About" },
+    NAVBAR_RESUME: { id: "#navbar-resume", value: "Resume" },
+    NAVBAR_SKILLS: { id: "#navbar-skills", value: "Skills" },
+    NAVBAR_PORTFOLIO: { id: "#navbar-portfolio", value: "Portfolio" },
+    NAVBAR_CONTACTS: { id: "#navbar-contacts", value: "Contacts" },
+
+    //Web Portfolio
+    P_WEB_TITLE: { id: "#web-portfolio-title", value: "Web Application Projects" },
+    P_WEB_CV: { id: "#web-portfolio-cv", value: "Website Project with personal CV" },
+    P_WEB_CV_DESCRIPTION: { id: "#web-portfolio-cv-description", value: "This project was set up to put into practice my knowledge in Web development and also to be able to share the repositories of works and projects carried out from courses, personal projects and Computer Engineering course subjects." },
+    P_WEB_CV_DIRECTORY: { id: "#web-portfolio-directory-cv", value: " Directory" },
+    P_WEB_CSHARP: { id: "#web-portfolio-csharp", value: "Web application development project with ASP.NET Core MVC" },
+    P_WEB_CSHARP_DESCRIPTION: { id: "#web-portfolio-csharp-description", value: "Project developed during Udemy's C# COMPLETO Programação Orientada a Objetos + Projetos course, simulating a sales website. The intent was to learn the fundamentals and use the ASP.NET Core MVC Framework." },
+    P_WEB_CSHARP_DIRECTORY: { id: "#web-portfolio-directory-csharp", value: " Directory" },
+    P_WEB_VUE: { id: "#web-portfolio-vue", value: "Front-end application development project using Vue Framework" },
+    P_WEB_VUE_DESCRIPTION: { id: "#web-portfolio-vue-description", value: "Project developed in the Web Development subject using the Vue Framework." },
+    P_WEB_VUE_DIRECTORY: { id: "#web-portfolio-directory-vue", value: " Directory" },
+}
---
+++ js/languages/portuguese.js
@@ -0,0 +1,127 @@
+export const portugueseLanguage = {
+    //Navbar texts
+    NAVBAR_HOME: { id: "#navbar-home", value: "Início" },
+    NAVBAR_ABOUT: { id: "#navbar-about", value: "Sobre" },
+    NAVBAR_RESUME: { id: "#navbar-resume", value: "Resumo" },
+    NAVBAR_SKILLS: { id: "#navbar-skills", value: "Habilidades" },
+    NAVBAR_PORTFOLIO: { id: "#navbar-portfolio", value: "Portfólio" },
+    NAVBAR_CONTACTS: { id: "#navbar-contacts", value: "Contatos" },
+
+    //Home Script
+    TYPED_SCRIPT: { id: "#typed-script", value: "Estudante de Engenharia da Computação, buscando me tornar Desenvolvedor de Software e Web." },
+
+    //About Section
+    ABOUT_TITTLE: { id: "#about-title", value: "Sobre" },
+    ABOUT_OBJECTIVE: { id: "#about-objective", value: "Objetivo Profissional: Atuar no mercado como Desenvolvedor de Software e Web." },
+    ABOUT_DESCRIPTION: { id: "#about-description", value: "Atualmente cursando graduação em Engenharia da Computação na FACENS. Atuando como estagiário em Desenvolvimento de Software na FIT-Instituto de Tecnologia, onde participo do desenvolvimento de softwares utilizando C#.NET e WPF. Um ano e seis meses de experiência na área de TI, exercendo como atividades gestão, manutenção e montagem de equipamentos, suporte aos usuários do sistema operacional Windows e manutenção e suporte a infraestrutura de redes." },
+    ABOUT_BIRTH_DATE: { id: "#about-birth-date", value: "Data de Nascimento:" },
+    ABOUT_DATE: { id: "#about-date", value: "27 de Maio de 1996" },
+    ABOUT_CELLPHONE: { id: "#about-cellphone", value: "Celular:" },
+    ABOUT_CITY: { id: "#about-city", value: "Cidade:" },
+    ABOUT_CITY_INFO: { id: "#about-city-info", value: "Sorocaba-SP Brasil" },
+    ABOUT_DEGREE: { id: "#about-degree", value: "Formação:" },
+    ABOUT_DEGREE_INFO: { id: "#about-degree-info", value: "Engenharia da Computação" },
+
+    //Resume section
+    RESUME_TITLE: { id: "#resume-title", value: "Resumo" },
+    RESUME_PROFESSIONAL_EXPERIENCE: { id: "#resume-professional-experience", value: "Experiência Profissional" },
+    RESUME_INTERN_DEV: { id: "#resume-intern-dev", value: "Estagiário de Desenvolvimento de Software" },
+    RESUME_INTERN_DEV_DATE: { id: "#resume-intern-dev-date", value: "Maio/2021 - Cargo atual" },
+    RESUME_INTERN_DEV_DESCRIPTION: { id: "#resume-intern-dev-description", value: "Realização de tarefas relacionadas ao desenvolvimento de software nas linguagens de C# e WPF, correção de bugs e realização de testes, suporte à equipe de desenvolvimento de software, acompanhamento da rotina com a metodologia Scrum na realização de tasks e participação de reuniões de alinhamento e planejamentos." },
+    RESUME_IT_ANALYST: { id: "#resume-it-analyst", value: "Analista de TI" },
+    RESUME_IT_ANALYST_DATE: { id: "#resume-it-analyst-date", value: "Jan/2021 - Abr/2021" },
+    RESUME_IT_ANALYST_DESCRIPTION: { id: "#resume-it-analyst-description", value: "Suporte aos usuários do sistema operacional Windows, manutenção e acompanhamento de servidores, manutenção e suporte da infraestrutura de redes, realização de orçamento para compra de equipamentos, manutenção e montagem de equipamentos." },
+    RESUME_IT_ASSISTANT: { id: "#resume-it-assistant", value: "Assistente de TI" },
+    RESUME_IT_ASSISTANT_DATE: { id: "#resume-it-assistant-date", value: "Fev/2020 - Dez/2020" },
+    RESUME_IT_ASSISTANT_DESCRIPTION: { id: "#resume-it-assistant-description", value: "Manutenção e montagem de equipamentos, suporte aos usuários do sistema operacional Windows e manutenção e suporte da infraestrutura de redes." },
+    RESUME_IT_INTERN: { id: "#resume-it-intern", value: "Estagiário de TI" },
+    RESUME_IT_INTERN_DATE: { id: "#resume-it-intern-date", value: "Nov/2019 - Jan/2020" },
+    RESUME_IT_INTERN_DESCRIPTION: { id: "#resume-it-intern-description", value: "Auxílio técnico em sistemas, suporte ao usuário e apoio à equipe de TI." },
+    RESUME_ACADEMIC_BACKGROUND: { id: "#resume-academic-background", value: "Formação Acadêmica" },
+    RESUME_COMPUTER_ENGINEERING: { id: "#resume-degree-ce", value: "Graduação em Engenharia da Computação - FACENS" },
+    RESUME_COMPUTER_ENGINEERING_DATE: { id: "#resume-degree-ce-date", value: "Jun/2019 - Previsão de conclusão: Dez/2023" },
+    RESUME_COMPUTER_ENGINEERING_lOCALIZATION: { id: "#resume-degree-ce-localization", value: "Sorocaba-SP Brasil" },
+    RESUME_LEAN_SIX_SIGMA: { id: "#resume-degree-lss", value: "Pós-Graduação em Especialização em Lean Six Sigma com certificação Black Belt - FACENS" },
+    RESUME_LEAN_SIX_SIGMA_DATE: { id: "#resume-degree-lss-date", value: "Mar/2019 - Jun/2021" },
+    RESUME_LEAN_SIX_SIGMA_lOCALIZATION: { id: "#resume-degree-lss-localization", value: "Sorocaba-SP Brasil" },
+    RESUME_CHEMICAL_ENGINEERING: { id: "#resume-degree-qe", value: "Graduação em Engenharia Química - FACENS" },
+    RESUME_CHEMICAL_ENGINEERING_DATE: { id: "#resume-degree-qe-date", value: "Jan/2014 - Dez/2018" },
+    RESUME_CHEMICAL_ENGINEERING_lOCALIZATION: { id: "#resume-degree-qe-localization", value: "Sorocaba-SP Brasil" },
+    RESUME_COURSES: { id: "#resume-courses", value: "Cursos" },
+    RESUME_COURSES_CSHARP_HOURS: { id: "#resume-courses-csharp_hours", value: "(37 horas)" },
+    RESUME_COURSES_CSHARP_CERTIFICATE: { id: "#resume-courses-csharp-certificate", value: " Certificado" },
+    RESUME_COURSES_LINUX_HOURS: { id: "#resume-courses-linux_hours", value: "(8,5 horas)" },
+    RESUME_COURSES_LINUX_CERTIFICATE: { id: "#resume-courses-linux-certificate", value: " Certificado" },
+    
+    //Skills section
+    SKILLS_TITLE: { id: "#skills-title", value: "Habilidades" },
+    SKILLS_FRAMEWORKS_PROGRAMING: { id: "#skills-frameworks-programing", value: "Conhecimento em Linguagens de Programação, Linguagens de Estrutura e Frameworks." },
+    SKILLS_HTML: { id: "#skills-html", value: " - Intermediário" },
+    SKILLS_CSS: { id: "#skills-css", value: " - Intermediário" },
+    SKILLS_CSHARP: { id: "#skills-csharp", value: " - Intermediário" },
+    SKILLS_C:{ id: "#skills-c", value: " - Básico" },
+    SKILLS_JS:{ id: "#skills-js", value: " - Básico" },
+    SKILLS_JAVA: { id: "#skills-java", value: " - Intermediário" },
+    SKILLS_ANGULAR:{ id: "#skills-angular", value: " - Básico" },
+    SKILLS_VUE:{ id: "#skills-vue", value: " - Básico" },
+    SKILLS_WPF:{ id: "#skills-wpf", value: " - Básico" },
+    SKILLS_TOOL_KNOWLEDGE: { id: "#skills-tool-knowledge", value: "Conhecimento em Ferramentas de Desenvolvimento." },
+    SKILLS_GIT: { id: "#skills-git", value: " - Intermediário" },
+    SKILLS_VSCODE: { id: "#skills-vscode", value: " - Intermediário" },
+    SKILLS_SCRUM:{ id: "#skills-scrum", value: " - Básico" },
+    SKILLS_VS:{ id: "#skills-vs", value: " - Intermediário" },
+    SKILLS_LANGUAGES: { id: "#skills-languages", value: "Idiomas." },
+    SKILLS_PT:{ id: "#skills-pt", value: " - Nativo" },
+    SKILLS_EN: { id: "#skills-en", value: " - Intermediário" },
+
+    //Portfolio section
+    PORTFOLIO_TITLE: { id: "#portfolio-title", value: "Portfólio" },
+    PORTFOLIO_SUBTITLE: { id: "#portfolio-subtitle", value: "Projetos e trabalhos realisados a partir de cursos, projetos pessoais e matérias do curso de Engenharia da Computação." },
+    PORTFOLIO_SOFTWARES: { id: "#portfolio-softwares", value: "-Projetos de Software" },
+    PORTFOLIO_WEB: { id: "#portfolio-web", value:"-Projetos de Aplicações Web" },
+
+    //Contacts section
+    CONTACTS_TITLE: { id: "#contacts-title", value: "Contatos" },
+    CONTACTS_SUBTITLE: { id: "#contacts-subtitle", value: "Segue abaixo meus contatos pessoais:" },
+    CONTACTS_LOCALIZATION: { id: "#contacts-localization", value: "Localização:" },
+    CONTACTS_LOCALIZATION_INFO: { id: "#contacts-localization-info", value: "Sorocaba-SP Brasil" },
+    CONTACTS_CELLPHONE: { id: "#contacts-cellphone", value: "Celular:" },
+}
+
+export const portugueseLanguagesoft = {
+    //Navbar texts
+    NAVBAR_HOME: { id: "#navbar-home", value: "Início" },
+    NAVBAR_ABOUT: { id: "#navbar-about", value: "Sobre" },
+    NAVBAR_RESUME: { id: "#navbar-resume", value: "Resumo" },
+    NAVBAR_SKILLS: { id: "#navbar-skills", value: "Habilidades" },
+    NAVBAR_PORTFOLIO: { id: "#navbar-portfolio", value: "Portfólio" },
+    NAVBAR_CONTACTS: { id: "#navbar-contacts", value: "Contatos" },
+    
+    //Software Portfolio
+    P_SOFTWARE_TITLE: { id: "#software-portfolio-title", value: "Projetos de Software" },
+    P_SOFTWARE_CHESS: { id: "#software-portfolio-chess", value: "Projeto de Xadrez" },
+    P_SOFTWARE_CHESS_DESCRIPTION: { id: "#software-portfolio-chess-description", value: "Projeto desenvolvido durante o curso de C# COMPLETO Programação Orientada a Objetos + Projetos, simulando o jogo de xadrez clássico. Aplicando os conhecimentos de encapsulamento, herança, polimorfismo, enumeração e tratamento de exceptions." },
+    P_SOFTWARE_CHESS_DIRECTORY: { id: "#software-portfolio-directory-chess", value: "Directório" },   
+}
+
+export const portugueseLanguageweb = {
+    //Navbar texts
+    NAVBAR_HOME: { id: "#navbar-home", value: "Início" },
+    NAVBAR_ABOUT: { id: "#navbar-about", value: "Sobre" },
+    NAVBAR_RESUME: { id: "#navbar-resume", value: "Resumo" },
+    NAVBAR_SKILLS: { id: "#navbar-skills", value: "Habilidades" },
+    NAVBAR_PORTFOLIO: { id: "#navbar-portfolio", value: "Portfólio" },
+    NAVBAR_CONTACTS: { id: "#navbar-contacts", value: "Contatos" },
+   
+    //Web Portfolio
+    P_WEB_TITLE: { id: "#web-portfolio-title", value: "Projetos de Aplicações Web" },
+    P_WEB_CV: { id: "#web-portfolio-cv", value: "Projeto de site com CV pessoal" },
+    P_WEB_CV_DESCRIPTION: { id: "#web-portfolio-cv-description", value: "Este projeto foi montado para colocar em prática meus conhecimentos em desenvolvimento Web e de também poder compartilhar os repositórios de trabalhos e projetos realisados a partir de cursos, projetos pessoais e matérias do curso de Engenharia da Computação." },
+    P_WEB_CV_DIRECTORY: { id: "#web-portfolio-directory-cv", value: " Diretório" },
+    P_WEB_CSHARP: { id: "#web-portfolio-csharp", value: "Projeto de desenvolvimento de aplicação web com ASP.NET Core MVC" },
+    P_WEB_CSHARP_DESCRIPTION: { id: "#web-portfolio-csharp-description", value: "Projeto desenvolvido durante o curso da Udemy de C# COMPLETO Programação Orientada a Objetos + Projetos, simulando um site de vendas. O intuito foi de conhecer os fundamentos e utilizar o Framework ASP.NET Core MVC." },
+    P_WEB_CSHARP_DIRECTORY: { id: "#web-portfolio-directory-csharp", value: " Diretório" },
+    P_WEB_VUE: { id: "#web-portfolio-vue", value: "Projeto de desenvolvimento de aplicação front utilizando Framework Vue" },
+    P_WEB_VUE_DESCRIPTION: { id: "#web-portfolio-vue-description", value: "Projeto desenvolvido na matéria de Desenvolvimento Web utilizando o Framework Vue." },
+    P_WEB_VUE_DIRECTORY: { id: "#web-portfolio-directory-vue", value: " Diretório" },
+}
